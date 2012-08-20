d00fecf1b0febfba946b65125da707e46e2536bf
Joris Dauphin <Jarod42@users.noreply.github.com>
2012-08-21 00:37:38 +0200

    Empty project

---
+++ premake/buildall.bat
@@ -0,0 +1,8 @@
+@set premake_exe=Premake4.exe
+@set premake_entry=premake4.lua
+
+"%premake_exe%" --file="%premake_entry%" codelite
+"%premake_exe%" --file="%premake_entry%" codeblocks
+@rem "%premake_exe%" --file="%premake_entry%" vs2005
+
+@pause
