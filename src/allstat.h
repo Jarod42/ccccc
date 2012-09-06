f14551de74671c34173b93e67b0c9752672c8c05
Joris Dauphin <Jarod42@users.noreply.github.com>
2012-09-06 11:28:00 +0200

    missing files.

---
+++ src/allstat.h
@@ -0,0 +1,21 @@
+#ifndef ALL_STAT_H
+#define ALL_STAT_H
+
+#include <vector>
+
+#include "filestat.h"
+
+class AllStat
+{
+public:
+	void Compute(const char* filename, int extraArgsCount, const char *extraArgs[]);
+
+	unsigned int getFileCount() const { return m_filesStat.size(); }
+	const FileStat& getFileStat(unsigned int index) const { return m_filesStat[index]; }
+
+private:
+	std::vector<FileStat> m_filesStat;
+};
+
+
+#endif // ALL_STAT_H
