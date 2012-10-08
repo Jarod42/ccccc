75d4e6c9b82f91b844d4e43b0cb68d1736f25a37
Joris Dauphin <Jarod42@users.noreply.github.com>
2012-10-08 16:40:29 +0200

    Test Parameters::Parse (long/short options).

--- test/test_parameters.cpp
+++ test/test_parameters.cpp
@@ -6,7 +6,7 @@
 #define ARRAY_SIZE(a) (sizeof (a) / sizeof (*a))
 
 template <typename Iterator1, typename Iterator2>
-bool IsRangeEq(Iterator1 begin1, Iterator1 end1, Iterator2 begin2, Iterator2 end2)
+static bool IsRangeEq(Iterator1 begin1, Iterator1 end1, Iterator2 begin2, Iterator2 end2)
 {
 	if (end1 - begin1 != end2 - begin2) {
 		return false;
@@ -73,5 +73,61 @@ TEST(PARAMETERS_ADD_PCH)
 	Parameters param;
 
 	param.SetPch(pchFile);
+	CHECK_EQUAL(pchFile, param.GetPch());
+}
+
+TEST(PARAMETERS_PARSING_SHORT_OPTIONS)
+{
+	const char* argv0 = "ccccc_test";
+	const char* includeFlag = "-I";
+	const char* defineFlag = "-D";
+	const char* extraFlag = "-e";
+	const char* pchFlag = "-p";
+	std::string files[] = {"a.c", "a.h"};
+	std::string includes[] = {"includeDir1", "includeDir2"};
+	std::string defines[] = {"FOO", "BAR=42"};
+	std::string extras[] = {"-std=c++0x"};
+	std::string pchFile = "pchFile";
+	const char *argv[] = {argv0,
+		includeFlag, includes[0].c_str(), defineFlag, defines[0].c_str(), extraFlag, extras[0].c_str(),
+		pchFlag, pchFile.c_str(), defineFlag, defines[1].c_str(), includeFlag, includes[1].c_str(),
+		files[0].c_str(), files[1].c_str()
+	};
+	Parameters param;
+
+	param.Parse(ARRAY_SIZE(argv), const_cast<char **>(argv));
+
+	CHECK(IsRangeEq(files, files + ARRAY_SIZE(files), param.Files_begin(), param.Files_end()));
+	CHECK(IsRangeEq(includes, includes + ARRAY_SIZE(includes), param.IncludePaths_begin(), param.IncludePaths_end()));
+	CHECK(IsRangeEq(defines, defines + ARRAY_SIZE(defines), param.Defines_begin(), param.Defines_end()));
+	CHECK(IsRangeEq(extras, extras + ARRAY_SIZE(extras), param.Extras_begin(), param.Extras_end()));
+	CHECK_EQUAL(pchFile, param.GetPch());
+}
+
+TEST(PARAMETERS_PARSING_LONG_OPTIONS)
+{
+	const char* argv0 = "ccccc_test";
+	const char* includeFlag = "--include-dir";
+	const char* defineFlag = "--define";
+	const char* extraFlag = "--extra-option";
+	const char* pchFlag = "--pch";
+	std::string files[] = {"a.c", "a.h"};
+	std::string includes[] = {"includeDir1", "includeDir2"};
+	std::string defines[] = {"FOO", "BAR=42"};
+	std::string extras[] = {"-std=c++0x"};
+	std::string pchFile = "pchFile";
+	const char *argv[] = {argv0,
+		includeFlag, includes[0].c_str(), defineFlag, defines[0].c_str(), extraFlag, extras[0].c_str(),
+		pchFlag, pchFile.c_str(), defineFlag, defines[1].c_str(), includeFlag, includes[1].c_str(),
+		files[0].c_str(), files[1].c_str()
+	};
+	Parameters param;
+
+	param.Parse(ARRAY_SIZE(argv), const_cast<char **>(argv));
+
+	CHECK(IsRangeEq(files, files + ARRAY_SIZE(files), param.Files_begin(), param.Files_end()));
+	CHECK(IsRangeEq(includes, includes + ARRAY_SIZE(includes), param.IncludePaths_begin(), param.IncludePaths_end()));
+	CHECK(IsRangeEq(defines, defines + ARRAY_SIZE(defines), param.Defines_begin(), param.Defines_end()));
+	CHECK(IsRangeEq(extras, extras + ARRAY_SIZE(extras), param.Extras_begin(), param.Extras_end()));
 	CHECK_EQUAL(pchFile, param.GetPch());
 }
