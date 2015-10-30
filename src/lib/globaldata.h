e554672bf6f68c15eb4c597bcc4093a63062654c
Jarod42 <Jarod42@localhost>
2015-10-30 22:45:04 +0100

    Add Missing file.

---
+++ src/lib/globaldata.h
@@ -0,0 +1,36 @@
+/*
+** Copyright 2015 Joris Dauphin
+*/
+/*
+**  This file is part of CCCCC.
+**
+**  CCCCC is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  CCCCC is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with CCCCC. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GLOBAL_DATA_H
+#define GLOBAL_DATA_H
+
+#include "caller_count.h"
+
+namespace ccccc
+{
+
+struct GlobalData
+{
+	CallerCountData m_callerCountData;
+};
+
+}
+
+#endif // GLOBAL_DATA_H
