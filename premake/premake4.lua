735bab9ac429b38917083933b7858ba29dad3fda
Jarod42 <Jarod42@users.noreply.github.com>
2022-10-18 12:59:07 +0200

    Clean up premake4.lua

--- premake/premake4.lua
+++ premake/premake4.lua
@@ -3,15 +3,21 @@ Root = path.getabsolute("..")
 ThirdRoot = path.getabsolute("../3rd")
 
 -- Some path to customize with your config.
-UnitTestPPRoot = "C:/UnitTest++-1.3"
-CTemplateRoot = path.join(ThirdRoot, "ctemplate-2.4");
+--UnitTestPPRoot = "C:/UnitTest++-1.3"
+--CTemplateRoot = path.join(ThirdRoot, "ctemplate-2.4");
 
 -- You should not modify this script below this line.
 
 newoption {
-   trigger = "llvm-root",
-   value = "path",
-   description = "path of llvm root (contains lib include bin/llvm-config)"
+	trigger = "llvm-root",
+	value = "path",
+	description = "path of llvm root (contains lib include bin/llvm-config)"
+}
+
+-- tinfo is not part of msys
+newoption {
+	trigger = "without-tinfo",
+	description = "don't add tinfo library dependency"
 }
 
 if (_ACTION == nil) then
@@ -21,9 +27,13 @@ end
 LLVMRoot = _OPTIONS["llvm-root"]
 
 if (LLVMRoot == nil or LLVMRoot == "") then
-	print("No llvm-root provided")
-	print("Generation aborted")
-	return
+	-- assume llvm is installed in system
+	LLVMConfig = "llvm-config"
+else
+	LLVMIncludeDir = path.join(LLVMRoot, "include")
+	LLVMBinDir = path.join(LLVMRoot, "bin")
+	LLVMLibDir = path.join(LLVMRoot, "lib")
+	LLVMConfig = path.join(LLVMBinDir, "llvm-config")
 end
 
 
@@ -59,13 +69,6 @@ ConfigurationsData = {
 LocationDir = path.join(Root, "project/" .. ActionsData[_ACTION].Dir)
 
 
-LLVMIncludeDir = path.join(LLVMRoot, "include")
-LLVMBinDir = path.join(LLVMRoot, "bin")
-LLVMLibDir = path.join(LLVMRoot, "lib")
-
-UnitTestPPIncludeDir = path.join(UnitTestPPRoot, "src")
-UnitTestPPLibDir = "/usr/local/lib"
-
 function DefaultConfiguration()
 	compilerData = CompilerData[ActionsData[_ACTION].Compiler]
 	for config,data in pairs(ConfigurationsData) do
@@ -83,19 +86,26 @@ function DefaultConfiguration()
 end
 
 function UseCTemplate()
-	includedirs { path.join(CTemplateRoot, "src")}
-	libdirs { path.join(CTemplateRoot, ".libs") }
+	--includedirs { path.join(CTemplateRoot, "src")}
+	--libdirs { path.join(CTemplateRoot, ".libs") }
 	links { "ctemplate_nothreads" }
 end
 
 function Llvm_config_cpp_flags()
-	buildoptions { "$(shell " .. path.join(LLVMBinDir, "llvm-config") .. " --cppflags" .. ")" }
+	buildoptions { "$(shell " .. LLVMConfig .. " --cppflags" .. ")" }
 end
 
 function LinkToClang()
 	configuration "*WithDLL"
-		links { "clang", "LLVMSupport", "tinfo" }
-		linkoptions { "$(shell " .. path.join(LLVMBinDir, "llvm-config") .. " --system-libs --ldflags --libs support" .. ")" }
+		links { "clang", "LLVMSupport"}
+
+		if not (_OPTIONS["without-tinfo"]) then
+			links { "tinfo" }
+		end
+
+		if (ActionsData[_ACTION].Compiler ~= "vc") then
+			linkoptions { "$(shell " .. LLVMConfig .. " --system-libs --ldflags --libs support" .. ")" }
+		end
 		linkoptions { "-pthread" }
 
 	configuration "not *WithDLL"
@@ -103,11 +113,24 @@ function LinkToClang()
 				"clangToolingCore", "clangFrontend", "clangDriver",
 				"clangSerialization", "clangParse", "clangSema",
 				"clangAnalysis", "clangRewrite", "clangEdit",
-				"clangAST", "clangLex", "clangBasic", "LLVMSupport", "tinfo"}
+				"clangAST", "clangLex", "clangBasic", "LLVMSupport"}
+		if not (_OPTIONS["without-tinfo"]) then
+			links { "tinfo" }
+		end
 
-		linkoptions { "$(shell " .. path.join(LLVMBinDir, "llvm-config") .. " --system-libs --ldflags --libs all support" .. ")" }
+		if (ActionsData[_ACTION].Compiler ~= "vc") then
+			linkoptions { "$(shell " .. LLVMConfig .. " --system-libs --ldflags --libs all support" .. ")" }
+		end
 		linkoptions { "-pthread" }
+	configuration "*"
+end
 
+--UnitTestPPIncludeDir = path.join(UnitTestPPRoot, "src")
+--UnitTestPPLibDir = "/usr/local/lib"
+function UseUnitTestPP()
+	--includedirs {UnitTestPPIncludeDir}
+	--libdirs {UnitTestPPLibDir}
+	links { "UnitTest++" }
 end
 
 
@@ -115,13 +138,13 @@ solution "ccccc"
 	location ( LocationDir )
 	configurations { "DebugWithDLL", "ReleaseWithDLL", "Debug", "Release" }
 
-	includedirs(LLVMIncludeDir)
+	if (LLVMIncludeDir ~= nil and LLVMIncludeDir ~= "") then includedirs(LLVMIncludeDir) end
 
 	configuration "*WithDLL"
-		libdirs(LLVMBinDir)
-		libdirs(LLVMLibDir)
+		if (LLVMBinDir ~= nil and LLVMBinDir ~= "") then libdirs(LLVMBinDir) end
+		if (LLVMLibDir ~= nil and LLVMLibDir ~= "") then libdirs(LLVMLibDir) end
 	configuration "not *WithDLL"
-		libdirs(LLVMLibDir)
+		if (LLVMLibDir ~= nil and LLVMLibDir ~= "") then libdirs(LLVMLibDir) end
 
 -- --------------------------------------
 	project "ccccc_app"
@@ -140,7 +163,6 @@ solution "ccccc"
 		links { "ccccc_lib"}
 		LinkToClang()
 
-		configuration "*"
 		if (_PREMAKE_VERSION >= "4.4") then
 			--debugdir(Root)
 			debugargs {"../../../samples/namespace.cpp"}
@@ -168,16 +190,13 @@ solution "ccccc"
 		flags { "ExtraWarnings", "FatalWarnings"}
 
 		includedirs { path.join(Root, "src/lib/") }
-		includedirs {UnitTestPPIncludeDir}
-		libdirs {UnitTestPPLibDir}
 
 		Llvm_config_cpp_flags()
 
 		links { "ccccc_lib" }
-		links { "UnitTest++" }
 		LinkToClang()
+		UseUnitTestPP()
 
-		configuration "*"
 		if (_PREMAKE_VERSION >= "4.4") then
 			--debugdir(Root)
 		end
@@ -187,12 +206,10 @@ solution "ccccc"
 -- --------------------------------------
 	if (_ACTION ~= "gmake") then -- project to see examples
 	project "sample"
-		kind "StaticLib"
+		kind "StaticLib" -- "None"
 		language "C++"
-		--flags "WinMain"
 		files { path.join(Root, "samples/**.*") }
 		flags { "ExtraWarnings", "FatalWarnings"}
-		--buildoptions "-std=c++14"
 
 		DefaultConfiguration()
 	end
