b28829838285a16a87ff246d4f7948435ec8a5ac
Jarod42 <Jarod42@users.noreply.github.com>
2024-05-15 01:47:38 +0200

    Update buildall.bat: select gcc for codelite, remove CodeBlocks

--- premake/buildall.bat
+++ premake/buildall.bat
@@ -1,9 +1,8 @@
 @set premake_exe=premake5.exe
 @set premake_entry=premake5.lua
 
-"%premake_exe%" --file="%premake_entry%" codelite --without-tinfo
-"%premake_exe%" --file="%premake_entry%" codeblocks --without-tinfo
+"%premake_exe%" --file="%premake_entry%" codelite --without-tinfo --cc=gcc
 "%premake_exe%" --file="%premake_entry%" gmake2 --without-tinfo
-@rem "%premake_exe%" --file="%premake_entry%" vs2019
+@rem "%premake_exe%" --file="%premake_entry%" vs2022
 
 @pause
